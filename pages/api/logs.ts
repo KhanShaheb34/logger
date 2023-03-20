eaf658016f455818a93476d2c7b916e98073d3d1
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-03-20 23:40:30 +0600

    feat: add redis support

--- pages/api/logs.ts
+++ pages/api/logs.ts
@@ -1,14 +1,30 @@
+import { client } from '@/src/lib/redis';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'POST') {
     const { logs } = req.body;
 
-    console.log(logs);
-    return res.status(200).json({ success: true });
+    try {
+      if (!client.isOpen) await client.connect();
+      await client.rPush('logs', logs);
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ success: false });
+    }
   } else if (req.method === 'GET') {
-    console.log('GET');
-    return res.status(200).json({ success: true });
+    try {
+      if (!client.isOpen) await client.connect();
+      const logs = await client.lRange('logs', 0, -1);
+      return res.status(200).json({ success: true, logs });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ success: false });
+    }
   }
 
   return res.status(405).json({ success: false });
