65733fa9908aa72b6dcf7c28acc3cffd4f35031e
KhanShaheb34 <shakirulhkhan@gmail.com>
2023-04-11 03:38:33 +0600

    style and search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,30 +29,48 @@ const Home = ({
   })[];
 }) => {
   const [selectedLog, setSelectedLog] = useState(logs[0]);
+  const [searchKeyword, setSearchKeyword] = useState("");
+
+  const filteredLogs = logs.filter(
+    (log) =>
+      log.message.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+      log.tag?.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+      log.originatedFrom.toLowerCase().includes(searchKeyword.toLowerCase())
+  );
 
   return (
     <main className="flex w-full ">
-      <div className="w-1/2 grow  p-3">
+      <div className="flex w-1/2  grow flex-col gap-5 p-5">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full rounded-md border border-gray-300 p-2"
+          onChange={(e) => setSearchKeyword(e.target.value)}
+        />
         <table className="w-full">
           <thead className="bg p-6">
-            <tr>
-              <th>createdAt</th>
-              <th>message</th>
-              <th>Originated From</th>
-              <th>Tag</th>
+            <tr className="border-b border-gray-500">
+              <th className="py-2">CreatedAt</th>
+              <th className="py-2">Message</th>
+              <th className="py-2">Originated From</th>
+              <th className="py-2">Tag</th>
             </tr>
           </thead>
           <tbody>
-            {logs.map((log) => (
+            {filteredLogs.map((log) => (
               <tr
                 key={log.id}
                 onClick={() => setSelectedLog(log)}
-                className="cursor-pointer"
+                className="cursor-pointer whitespace-nowrap border-b border-gray-200 hover:bg-slate-100"
               >
-                <td>{log.createdAt}</td>
-                <td>{log.message.slice(0, 30).replace("\n", " ") + "..."}</td>
-                <td>{log.originatedFrom.slice(0, 30) + "..."}</td>
-                <td>{log.tag}</td>
+                <td className="py-2">{log.createdAt}</td>
+                <td className="py-2">
+                  {log.message.slice(0, 30).replace("\n", " ") + "..."}
+                </td>
+                <td className="py-2">
+                  {log.originatedFrom.slice(0, 30) + "..."}
+                </td>
+                <td className="py-2">{log.tag}</td>
               </tr>
             ))}
           </tbody>
